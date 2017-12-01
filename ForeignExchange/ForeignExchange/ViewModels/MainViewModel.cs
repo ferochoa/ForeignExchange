1d7686a9f72340d6088f1db13e2451fb85edeb65
Fer ochoa <fer.ochoa.ar@gmail.com>
2017-12-01 10:35:59 -0300

    1/12

--- ForeignExchange/ForeignExchange/ViewModels/MainViewModel.cs
+++ ForeignExchange/ForeignExchange/ViewModels/MainViewModel.cs
@@ -5,23 +5,68 @@
     using System.Collections.ObjectModel;
     using System.Windows.Input;
     using System;
+    using System.ComponentModel;
 
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+        #region Attributes
+        bool _isRunning;
+        string _result;
+        #endregion
         #region Properties
         public string Amount { get; set; }
         public ObservableCollection<Rate> Rates{ get; set; }
         public Rate SourceRate { get; set; }
         public Rate TargetRate { get; set; }
-        public bool IsRunning { get; set; }
+        public bool IsRunning
+        {
+            get
+            {
+                return _isRunning;
+            }
+            set
+            {
+                if(_isRunning != value)
+                {
+                    _isRunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsRunning)));
+                }
+            }
+        }
         public bool IsEnabled { get; set; }
-        public string Result { get; set; }
+        public string Result
+        {
+            get
+            {
+                return _result;
+            }
+            set
+            {
+                if (_result != value)
+                {
+                    _result = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Result)));
+                }
+            }
+        }
 
         #endregion
+        #region Constructors
         public MainViewModel()
         {
-
+            LoadRates();
+        }        
+        #endregion       
+        #region Methods
+        void LoadRates()
+        {
+            IsRunning = true;
+            Result = "Loading rates...";
         }
+        #endregion
         #region Commands
         public ICommand ConvertCommand
         {
